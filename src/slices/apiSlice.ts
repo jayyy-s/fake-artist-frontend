28bb8b89aae683255611b22da9e8a4ed06c15605
jayyy-s <notactuallyjay@gmail.com>
2024-03-22 07:37:30 -0400

    use proxy

--- src/slices/apiSlice.ts
+++ src/slices/apiSlice.ts
@@ -1,9 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl =
-  import.meta.env.VITE_ENVIRONMENT === "development"
-    ? ""
-    : import.meta.env.VITE_API_URL;
+const baseUrl = "";
 
 const baseQuery = fetchBaseQuery({ baseUrl: baseUrl }); // TODO: fix proxy server not replacing in vite.config.ts
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
     proxy: {
       "/dev": {
         target: "http://localhost:5000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/dev /, ""),
       },
       "/api": {
         target: "https://fake-artist-backend.up.railway.app",
